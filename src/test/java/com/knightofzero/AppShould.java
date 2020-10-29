bad075cba7f392e7a13fcf3a0236fb06143d70e8
Jarne Amerlinck <r0817494@student.vives.be>
2020-10-29 20:31:16 +0100

    Empty project

---
+++ src/test/java/com/knightofzero/AppShould.java
@@ -0,0 +1,13 @@
+package com.knightofzero;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppShould {
+
+  @Test
+  void stupidTest() {
+    assertTrue(true);
+  }
+}
